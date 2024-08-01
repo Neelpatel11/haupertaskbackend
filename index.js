44027f8c8b76ad1f1c8857f3274680d002e38ca4
Neelpatel11 <83646676+Neelpatel11@users.noreply.github.com>
2024-08-01 23:02:47 +0530

    Port added

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ app.put('/restore-user/:id', async (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
